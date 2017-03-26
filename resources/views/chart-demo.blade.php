840514a2f0c3ccb8328908d88970680e1b2e5540
rodravid <roliveira@webeleven.com.br>
2017-03-26 10:03:05 -0300

    Demo for Chart from Blockchains

---
+++ resources/views/chart-demo.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html xmlns="BLUE HACK">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <![endif]-->
+    <title>BLUEHACK - Demo</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="/assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME ICON STYLE  -->
+    <link href="/assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- PRETTYPHOTO STYLE  -->
+    <link href="/assets/css/prettyPhoto.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE CSS -->
+    <link href="/assets/css/style.css" rel="stylesheet" />
+    <!-- CUSTOM THEME COLOR -- YOU CAN USE LIGHT & DARK THEMES  -->
+    <link href="/assets/css/themes/dark.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top scrollclass">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">BLOCKCHAIN +  AGRO</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#home-sec">Home</a></li>
+                <li><a href="#features-sec">Projeto</a></li>
+                <li><a href="#advance-sec">Serviços</a></li>
+            </ul>
+        </div>
+    
+    </div>
+</div>
+<div id="just-sec" style="min-height: 70vh;">
+    <div class="overlay">
+        Demonstração
+    </div>
+</div>
+<div id="just-sec" style="">
+    <div class="overlay">
+        AgroSolutions
+    </div>
+
+</div>
+<!--END JUST SECTION-->
+
+<section id="location-sec">
+    <div class="container">
+        <div class="row text-center min-set">
+            <div class="col-md-12">
+                <h2><mark class="head-glow">Nossa </mark>Localização </h2>
+                <hr class="sub-hr" />
+            </div>
+        </div>
+        <div class="row">
+            
+            <div class="col-md-12 text-center">
+                <h2>Bluehack</h2>
+                <br />
+                <h4>IBM BRASIL ,Rua Tutoia , 1234</h4>
+                <h4>São Paulo/SP </h4>
+                <h4>Brasil</h4>
+                <br />
+                
+                
+                <p>Todos direitos reservados</p>
+            </div>
+        </div>
+    
+    </div>
+</section>
+<!-- END LOCATION  SECTION -->
+<!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+<!-- CORE JQUERY  -->
+<script src="/assets/js/jquery-1.11.1.js"></script>
+<!-- BOOTSTRAP SCRIPTS  -->
+<script src="/assets/js/bootstrap.js"></script>
+<!-- PRETTYPHOTO SCRIPTS -->
+<script src="/assets/js/jquery.prettyPhoto.js"></script>
+<!-- PORTFOLIO FILTER PLUGIN  -->
+<script src="/assets/js/jquery.mixitup.min.js"></script>
+<!-- NICE SCROLL SCRIPTS   -->
+<script src="/assets/js/jquery.nicescroll.min.js"></script>
+<!-- EASING SCROLL SCRIPTS   -->
+<script src="/assets/js/jquery.easing.min.js"></script>
+<!-- CUSTOM SCRIPTS  -->
+<script src="/assets/js/custom.js"></script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chart-demo', function () {
+    return view('chart-demo');
+});
