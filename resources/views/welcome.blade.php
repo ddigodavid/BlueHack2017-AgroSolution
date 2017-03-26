00d073777f52773a4a8607012134cf8febeb8b18
rodravid <roliveira@webeleven.com.br>
2017-03-26 09:08:40 -0300

    New Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,263 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html xmlns="BLUE HACK">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <![endif]-->
+    <title>BLUE HACKATHON</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="/assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME ICON STYLE  -->
+    <link href="/assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- PRETTYPHOTO STYLE  -->
+    <link href="/assets/css/prettyPhoto.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE CSS -->
+    <link href="/assets/css/style.css" rel="stylesheet" />
+    <!-- CUSTOM THEME COLOR -- YOU CAN USE LIGHT & DARK THEMES  -->
+    <link href="/assets/css/themes/dark.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top scrollclass">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">BLOCKCHAIN +  AGRO</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#home-sec">Home</a></li>
+                <li><a href="#features-sec">Projeto</a></li>
+                <li><a href="#advance-sec">Serviços</a></li>
+            </ul>
+        </div>
+    
+    </div>
+</div>
+<!--END NAB BAR SECTION-->
+<div id="home-sec">
+    <div class="overlay">
+        <div class="container">
+            <div class="col-md-8 pad-top scrollclass">
+                
+                <h1>
+                    <strong>BLOCKACHAIN no AGRO é o futuro do campo</strong>
+                </h1>
+                <p class="home-p">
+                
+                
+                
+                </p>
+            </div>
+            <div class="col-md-4 img-home-side ">
+            
+            </div>
+        </div>
+    </div>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
+</div>
+<!--END HOME SECTION-->
+<section id="features-sec">
+    <div class="container">
+        <div class="row text-center min-set">
+            <div class="col-md-12">
+                <h2> SISTEMA INTEGRADO</h2>
+                <hr class="sub-hr" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 text-center">
+                <img src="/assets/img/blockchain2.jpeg" class="img-set" />
+                <h3>BLOCKCHAIN</h3>
+                <hr/>
+                <p>
+                    Todas mercadorias são validadas e totalmente seguradas com blockchain
+                </p>
+            </div>
+            
+            
+            <div class="col-md-4 text-center">
+                <img src="/assets/img/iot.png"
+                     class="img-set" />
+                <h3>IOT</h3>
+                <hr /
+                <p>
+                    Todas mercadorias analisadas durante todac cadeia produtiva até o descarte das embalagens
+                </p>
+            </div>
+            
+            <div class="col-md-4 text-center">
+                <img src="/assets/img/chatbot.png"
+                     class="img-set" />
+                <h3>CHATBOT</h3>
+                <hr /
+                <p>
+                    Todo processo pode ser analisado e consultato a todo momento por Chatbot
+                </p>
+            </div>
+        
+        </div>
+    </div>
+    
+    </div>
+</section>
+<!--END FEATURES SECTION-->
+<section id="skill-sec">
+    <div class="overlay">
+        <div class="container">
+            <div class="row ">
+                <div class="col-lg-3 col-md-3 col-sm-3 text-center ">
+                    <i class="fa fa-bullseye fa-5x"></i>
+                    <h4>Tecnologia Segura e Autônoma</h4>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="col-lg-3 col-md-3 col-sm-3 text-center ">
+                    <i class="fa fa-dollar fa-5x"></i>
+                    <h4>Eficiência Produtiva</h4>
                 </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 text-center ">
+                    <i class="fa fa-comments-o fa-5x"></i>
+                    <h4>Comunicação</h4>
+                </div>
+                
+                <div class="col-lg-3 col-md-3 col-sm-3 text-center">
+                    <i class="fa fa-user fa-5x"></i>
+                    <h4>Transparência</h4>
+                </div>
+            
+            </div>
+        </div>
+    </div>
+</section>
+<!--END SKILL SECTION-->
+<section id="advance-sec">
+    <div class="container">
+        <div class="row text-center min-set">
+            
+            <div class="col-md-12">
+                <h2> Serviço </h2>
+                <hr class="sub-hr" />
+            </div>
+        </div>
+        
+        
+        <div class="row">
+            <div class="col-md-4 text-center">
+                <img src="/assets/img/3.png" class="img-set" />
+                <h3>Análise do Produto</h3>
+                <p>
+                    Utilizando Blockchain, fazemos o acompanhamento de todo processo, desde dos bancos, à produção, logista e mesa. Com sistema totalmente seguro e automatizado
+                </p>
+            </div>
+            <div class="col-md-4 text-center">
+                <img src="/assets/img/5.png" class="img-set" />
+                <h3>Processo</h3>
+                <p>
+                    Temos um processo totalmente transparente, graças a uniao entre o Blockchain e IOT, registrando e analisando todos os dados
+                </p>
+            </div>
+            <div class="col-md-4 text-center">
+                <img src="/assets/img/6.png" class="img-set" />
+                <h3>Chatbot</h3>
+                <p>
+                
+                </p>
+            </div>
+        </div>
+    
+    </div>
+</section>
+<!--END ADVANCE SECTION-->
+<div id="just-sec" style="">
+    <div class="overlay">
+        AgroSolutions
+    </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+</div>
+<!--END JUST SECTION-->
+<section id="gallery-sec">
+    
+    
+    <div class="container ">
+        <div class="row text-center min-set">
+            <div class="col-md-12">
+                <h2><mark class="head-glow">Login </mark>Session </h2>
+                <hr class="sub-hr" />
+            </div>
+        </div>
+        <div class="container" style="margin-top:30px; height: 50%;">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h3 class="panel-title"><strong> </strong></h3></div>
+                    <div class="panel-body">
+                        <form role="form" action="#">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Usuario</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Insira seu usuario">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Senha <a href="/sessions/forgot_password">(esqueci minha senha)</a></label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Insira sua senha">
+                            </div>
+                            <center><button type="submit" class="btn btn-lg btn-default">Entrar</button></center>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</section>
+<!-- END GALLERY  SECTION -->
+
+<section id="location-sec">
+    <div class="container">
+        <div class="row text-center min-set">
+            <div class="col-md-12">
+                <h2><mark class="head-glow">Nossa </mark>Localização </h2>
+                <hr class="sub-hr" />
+            </div>
+        </div>
+        <div class="row">
+            
+            <div class="col-md-12 text-center">
+                <h2>Bluehack</h2>
+                <br />
+                <h4>IBM BRASIL ,Rua Tutoia , 1234</h4>
+                <h4>São Paulo/SP </h4>
+                <h4>Brasil</h4>
+                <br />
+                
+                
+                <p>Todos direitos reservados</p>
+            </div>
+        </div>
+    
+    </div>
+</section>
+<!-- END LOCATION  SECTION -->
+<!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+<!-- CORE JQUERY  -->
+<script src="/assets/js/jquery-1.11.1.js"></script>
+<!-- BOOTSTRAP SCRIPTS  -->
+<script src="/assets/js/bootstrap.js"></script>
+<!-- PRETTYPHOTO SCRIPTS -->
+<script src="/assets/js/jquery.prettyPhoto.js"></script>
+<!-- PORTFOLIO FILTER PLUGIN  -->
+<script src="/assets/js/jquery.mixitup.min.js"></script>
+<!-- NICE SCROLL SCRIPTS   -->
+<script src="/assets/js/jquery.nicescroll.min.js"></script>
+<!-- EASING SCROLL SCRIPTS   -->
+<script src="/assets/js/jquery.easing.min.js"></script>
+<!-- CUSTOM SCRIPTS  -->
+<script src="/assets/js/custom.js"></script>
+
+</body>
 </html>
